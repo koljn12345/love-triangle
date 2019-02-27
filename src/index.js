f7dc3fafcc4336a365331fea89c5a6852915a881
koljn <litovchenko_kolj@mail.ru>
2019-02-27 05:50:20 +0300

    ripped a love triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+   count=0;
+   for( i=0; i<preferences.length-2; i++) { 
+      if(preferences[i]==0) continue;
+      k=i;
+      for(j=0; j<=2; j++) {
+         if(preferences[k]==0 ||preferences[k]-1==k) { k=-1; break;}
+         k=preferences[k]-1;
+      }
+      if(k==i) {
+         count++; 
+         for(j=0; j<=2; j++) {
+         m=k;
+         k=preferences[k]-1;
+         preferences[m]=0;
+         }
+      }
+   }
+   
+   return count;
 };
